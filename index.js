f84b363ee7ded304ecb5ccce261c5c7d7e7a7fbb
geddy89 <generalgeddy@yahoo.com>
2022-01-30 21:10:12 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
-// Code your solution in this file!
+const hq = 42
+const block = 264
+function distanceFromHqInBlocks(pickup) {
+    //returns the number of blocks given a value
+   if (pickup > hq){
+   return pickup - hq;
+   }else if (pickup < hq){
+       return hq - pickup;
+   }
+  }
+
+
+
+function distanceFromHqInFeet(destination) {
+  if (hq > destination)
+    return (hq - destination) * 264;
+    else (hq < destination)
+    return (destination - hq) * 264;
+}
+
+
+function distanceTravelledInFeet(start, destination) {
+    if (start > destination)
+    return (start - destination) * 264;
+    else (start < destination)
+    return (destination - start) * 264;
+  }
+
+
+  function calculatesFarePrice(start, destination) {
+ let blockDistance = Math.abs(start - destination);
+ let distanceTraveled = blockDistance * 264;
+ if (distanceTraveled <= 400){
+   console.log ('give customers a free sample');
+   return 0
+ }
+else if (distanceTraveled >= 2500)
+return 'cannot travel that far';
+
+else if (2000 <= distanceTraveled && distanceTraveled <= 2500)
+return 25;
+
+ else if (400 <= distanceTraveled && distanceTraveled <= 2000) {
+  let ridecost =  (0.02 * (distanceTraveled - 400));
+ return (distanceTraveled -400)*0.02;
+}
+
+
+
+/*else if (distanceTraveled => 401 && distanceTraveled <=2000)
+return (distanceTraveled - 400)*0.02;*/
+  
+
+
+  }
